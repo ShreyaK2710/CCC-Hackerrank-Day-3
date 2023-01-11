859e7219a788665a3dbee1dfd78de59df96fa715
Shreya K <95535050+ShreyaK2710@users.noreply.github.com>
2023-01-11 23:54:08 +0530

    Create F 103 : Gets and Puts.c

---
+++ F 103 : Gets and Puts.c
@@ -0,0 +1,13 @@
+#include<stdio.h>
+#include<string.h>
+int main()
+{
+	char str[50];
+	//input string here using gets
+    gets(str);
+    puts(str);
+
+	//print string here using puts
+	
+	return 0;
+}
