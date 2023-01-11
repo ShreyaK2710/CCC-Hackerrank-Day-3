91afc311d14dfaf4fd1038e7a1063786a0a48734
Shreya K <95535050+ShreyaK2710@users.noreply.github.com>
2023-01-11 23:51:04 +0530

    Create F 102 : Input a String.c

---
+++ F 102 : Input a String.c
@@ -0,0 +1,13 @@
+#include<stdio.h>
+#include<string.h>
+int main()
+{
+	char str[50];
+	//input string here using scanf
+    scanf("%s",&str);
+    printf("%s",str);
+
+	//print string here using printf
+	
+	return 0;
+}
